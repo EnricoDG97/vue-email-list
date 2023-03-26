c69cfb2a7ab4ce9b756ce070614df2769010040e
EnricoDG97 <enrico.delgaudio97@gmail.com>
2023-03-26 16:32:01 +0200

    feat: added axios and api url, linked to html via vue

--- script.js
+++ script.js
@@ -2,9 +2,26 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            emails: [],
         }
     },
 
+    created() {
+        this.genEmails(10);
+    },
+
+    mounted () {
+
+    },
+
     methods: {
+        genEmails(quantity) {
+            for(let i=0; i < quantity; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+                    const mailGenerated = res.data.response;
+                    this.emails.push(mailGenerated);
+                });
+            }
+        },
     },
 }).mount('#app');
